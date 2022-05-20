082ed149c10dd816e2663859126eeaac0ea02471
nandini4575 <105492347+nandini4575@users.noreply.github.com>
2022-05-20 12:16:01 +0530

    Add files via upload

---
+++ boolean.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class pro6
+{
+	public static void main(String args[])
+	{
+		int num1,num2,res;
+		num1=23;
+		num2=34;
+		boolean b1 = (num1>num2);
+		boolean b2 = (num1<num2);
+		System.out.println(b1);
+		System.out.println(b2);
+	}
+}
